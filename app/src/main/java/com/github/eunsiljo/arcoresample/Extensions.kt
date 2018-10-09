fd335e6cb42d1c1b36cc18af1f06f6aabd8f4ca7
EunsilJo <ucccuccc@gmail.com>
2018-10-09 11:28:19 +0900

    1. Sceneform Overview

--- app/src/main/java/com/github/eunsiljo/arcoresample/Extensions.kt
+++ app/src/main/java/com/github/eunsiljo/arcoresample/Extensions.kt
@@ -7,7 +7,7 @@ import android.os.Build
 import android.widget.Toast
 
 fun Activity.checkIsSupportedDevice(): Boolean =
-    when (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+    when (Build.VERSION.SDK_INT < ARSupportedDevice.MIN_BUILD_VERSION_CODE) {
         true -> {
             Toast.makeText(this, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show()
             false
@@ -16,7 +16,7 @@ fun Activity.checkIsSupportedDevice(): Boolean =
             val openGlVersionString = (getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
                 .deviceConfigurationInfo
                 .glEsVersion
-            when (java.lang.Double.parseDouble(openGlVersionString) < SupportedDevice.MIN_OPENGL_VERSION) {
+            when (java.lang.Double.parseDouble(openGlVersionString) < ARSupportedDevice.MIN_OPENGL_VERSION) {
                 true -> {
                     Toast.makeText(this, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG).show()
                     false
@@ -26,6 +26,7 @@ fun Activity.checkIsSupportedDevice(): Boolean =
         }
     }
 
-object SupportedDevice {
+object ARSupportedDevice {
+    const val MIN_BUILD_VERSION_CODE: Int = Build.VERSION_CODES.N
     const val MIN_OPENGL_VERSION: Double = 3.0
 }
